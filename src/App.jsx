04dcdaa726eb61263126150aabdc3276d29cfa2e
FahadQ <Fhdq2003@email.com>
2025-08-11 16:52:32 +0300

    added links and route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-// src/App.jsx
+import { Route, BrowserRouter as Router, Routes } from "react-router";
+import Home from "./Home";
+import Navbar from "./components/Navbar/Navbar";
+import MailboxList from "./components/MailboxList/MailboxList";
+import MailboxForm from "./components/MailboxForm/MailboxForm";
+import MailboxDetails from "./components/MailboxDetails/MailboxDetails";
+import { useState } from "react";
 
 const App = () =>
 {
+  
+
   return (
-    <>
-      
-    </>
+    <Router>
+      <>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />}>Home</Route>
+          <Route path="/mailboxes" element={<MailboxList />}>Mailboxes</Route>
+          <Route path="/new-mailbox" element={<MailboxForm />}>New Mailbox</Route>
+          <Route path="/mailboxes/:mailboxId" element={<MailboxDetails />} />
+        </Routes>
+      </>
+    </Router>
   )
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router";
+import Home from "../../Home";
+import MailboxList from "../MailboxList/MailboxList";
+import MailboxForm from "../MailboxForm/MailboxForm";
+
 function Navbar()
 {
     return (
         <>
-            <h1>Navbar</h1>
+            <Link to="/" >Home</Link>
+            <Link to="/mailboxes" >Mailboxes</Link>
+            <Link to="/new-mailbox" >New Mailbox</Link>
         </>
     )
 }
